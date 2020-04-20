60cde8397331e312a5c594563832637b89423862
Szymon Sadowski <szymonsadowski3@gmail.com>
2020-04-20 21:50:27 +0200

    - renaming

--- test/continuous_streams_test.py
+++ test/continuous_streams_test.py
@@ -6,7 +6,7 @@ from src.rest_api.process.continuous_streams import find_continuous_streams
 
 
 class LearningCase(unittest.TestCase):
-    def test_continous_streams_finding(self):
+    def test_continuous_streams_finding(self):
         input_rows = [
             RealDictRow([('event_type', None), ('ip_src', None), ('ip_dst', 'TEST'), ('port_src', None), ('port_dst', None), ('timestamp_start', datetime.datetime(2020, 4, 19, 20, 9, 33)), ('timestamp_end', None), ('packets', None), ('bytes', None), ('writer_id', None), ('mac_src', None), ('mac_dst', None), ('ip_proto', None), ('src_hostname', None), ('dst_hostname', None), ('incoming_outgoing', None)]),
             RealDictRow([('event_type', None), ('ip_src', None), ('ip_dst', 'TEST'), ('port_src', None), ('port_dst', None), ('timestamp_start', datetime.datetime(2020, 4, 19, 20, 9, 48)), ('timestamp_end', None), ('packets', None), ('bytes', None), ('writer_id', None), ('mac_src', None), ('mac_dst', None), ('ip_proto', None), ('src_hostname', None), ('dst_hostname', None), ('incoming_outgoing', None)]),
