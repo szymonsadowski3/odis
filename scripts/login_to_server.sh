0aab542925a768193690e30519cf8454d01ef3b3
Szymon Sadowski <szymonsadowski3@gmail.com>
2020-05-24 17:18:53 +0200

    - fixed issue in finding streams of usage

---
+++ scripts/login_to_server.sh
@@ -0,0 +1,1 @@
+ssh -i "adminiarz.pem" ubuntu@ec2-52-59-48-240.eu-central-1.compute.amazonaws.com
--- src/rest_api/process/continuous_streams.py
+++ src/rest_api/process/continuous_streams.py
@@ -9,6 +9,12 @@ def get_stream_dictionary(stream_start, stream_end, total_time, records_in_strea
         "records_in_stream": records_in_stream,
     }
 
+def get_final_stream(current_stream):
+    stream_start = current_stream[0]["timestamp_start"]
+    stream_end = current_stream[-1]["timestamp_start"]
+    total_time_in_seconds = (stream_end - stream_start).seconds
+    stream_dictionary = get_stream_dictionary(stream_start, stream_end, total_time_in_seconds, current_stream)
+    return stream_dictionary
 
 def find_continuous_streams(records):
     streams = []
@@ -16,6 +22,7 @@ def find_continuous_streams(records):
     previous_record = None
     current_stream = []
     start_over = False
+    records_length = len(records)
 
     for index, record in enumerate(records):
         if (index == 0) or start_over:
@@ -27,11 +34,11 @@ def find_continuous_streams(records):
         if timedelta.seconds < 5*MINUTE:
             current_stream.append(record)
             previous_record = record
+            if index == records_length - 1:
+                stream_dictionary = get_final_stream(current_stream)
+                streams.append(stream_dictionary)
         else:
-            stream_start = current_stream[0]["timestamp_start"]
-            stream_end = current_stream[-1]["timestamp_start"]
-            total_time_in_seconds = (stream_end - stream_start).seconds
-            stream_dictionary = get_stream_dictionary(stream_start, stream_end, total_time_in_seconds, current_stream)
+            stream_dictionary = get_final_stream(current_stream)
             streams.append(stream_dictionary)
             current_stream = []
             start_over = True
